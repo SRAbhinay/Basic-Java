aa3b50f8d24616827b68a05439894a1502f63a3f
SRAbhinay <124809897+SRAbhinay@users.noreply.github.com>
2023-09-23 16:43:12 +0530

    Add files via upload

---
+++ Sorting/selectionsort.java
@@ -0,0 +1,22 @@
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 3, 4, 6};
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
