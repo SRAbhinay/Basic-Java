753186bbde88ff8ed7cfd45a313fc3711b364584
S Raj Abhinay <124809897+SRAbhinay@users.noreply.github.com>
2023-10-10 11:23:42 +0530

    Update Daysinmonth.java

--- Days in a month/Daysinmonth.java
+++ Days in a month/Daysinmonth.java
@@ -31,8 +31,7 @@ public class Main {
     }
     
     private static String getMonthName(int month) {
-        String[] monthNames = {
-            "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+        String[] monthNames = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
         return monthNames[month - 1];
     }
     
