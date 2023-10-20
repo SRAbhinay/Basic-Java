aa49a211e95a47b86306451b65dd344d4e7c6bbd
S Raj Abhinay <124809897+SRAbhinay@users.noreply.github.com>
2023-10-20 13:11:55 +0530

    Update tic-tac-toe.java

--- tic-tac-toe.java
+++ tic-tac-toe.java
@@ -14,6 +14,7 @@ void displayBoard() {
     std::cout << "+---+---+---+" << std::endl;
     
     for (int i = 0; i < 3; i++) {
+       
         for (int j = 0; j < 3; j++) {
             std::cout << "| " << board[i][j] << " ";
         }
